10815dd15e3ab4aab59226ad623203bdcda1e47c
pompydev <pompydev@proton.me>
2025-11-08 18:24:05 +0900

    web: remove drizzle (replaced with convex)

--- web/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js"
-import postgres from "postgres"
-
-import { env } from "../env"
-
-const pool = postgres(env.DATABASE_URL, { max: 1 })
-
-export const db = drizzle(pool)
