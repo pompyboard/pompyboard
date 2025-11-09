254ed314c1e5cd0aca2f2243e5eb25df6bf19209
pompydev <pompydev@proton.me>
2025-11-09 10:47:59 +0900

    web: remove unused db stuff (they belong in another PR)

--- web/convex/_generated/api.d.ts
+++ web/convex/_generated/api.d.ts
@@ -9,10 +9,7 @@
  */
 
 import type * as auth from "../auth.js";
-import type * as content from "../content.js";
 import type * as http from "../http.js";
-import type * as products from "../products.js";
-import type * as seed from "../seed.js";
 import type * as waitlist from "../waitlist.js";
 
 import type {
@@ -31,10 +28,7 @@ import type {
  */
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
-  content: typeof content;
   http: typeof http;
-  products: typeof products;
-  seed: typeof seed;
   waitlist: typeof waitlist;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
