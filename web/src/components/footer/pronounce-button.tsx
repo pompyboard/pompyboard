2ab11c2036ee694f4f1846dadef182b7024634ca
pompydev <pompydev@proton.me>
2025-11-23 07:38:00 +0900

    fix pronunciation button making unnecessary network requests

--- web/src/components/footer/pronounce-button.tsx
+++ web/src/components/footer/pronounce-button.tsx
@@ -1,15 +1,24 @@
 "use client"
 
 import { Icon } from "@iconify/react"
+import { useRef } from "react"
 
+// https://github.com/wonkleio/wonkle/pull/29
 export default function PronounceButton() {
-    // See https://github.com/wonkleio/wonkle/pull/29
+    const audioRef = useRef<HTMLAudioElement>(null)
+
     return (
-        <button
-            onClick={() => new Audio("/wonkle.mp3").play()}
-            className="cursor-pointer rounded px-2 py-1"
-        >
-            <Icon icon="mdi:speakerphone" className="inline-block h-5 w-5" />
-        </button>
+        <>
+            <button
+                className="cursor-pointer rounded px-2 py-1"
+                onClick={() => audioRef.current?.play()}
+            >
+                <Icon
+                    icon="mdi:speakerphone"
+                    className="inline-block h-5 w-5"
+                />
+            </button>
+            <audio ref={audioRef} src="/wonkle.mp3" className="hidden" />
+        </>
     )
 }
